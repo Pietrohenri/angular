b9cbb9c5f38ce8f7dc7f8e7b5126c46a5b4fa626
Pietro Henrique <henripietro@gmail.com>
2021-06-02 20:26:43 -0300

    teste

---
+++ hello-typescript/main.ts
@@ -0,0 +1,27 @@
+var minhaVar = ' minha variavel';
+
+function minhaFunc (x,y){
+    return x + y;
+
+}
+//Es 6 ou Es 2015
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1,2,3];
+numeros.map(function(valor) {
+    return  valor * 2 ;
+
+});
+
+numeros.map(valor => valor *2); //Es2015
+
+class Matematica {
+    soma (x,y){
+        return x + y;
+        
+    }
+}
+
+var n1: any  = 'sdfsdf';
+n1 = 4;
